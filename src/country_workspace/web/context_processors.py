ec444efb1023f8df51fd465681b85b069725ebde
Stefano Apostolico <s.apostolico@gmail.com>
2024-11-14 20:22:33 +0100

    footer

--- src/country_workspace/web/context_processors.py
+++ src/country_workspace/web/context_processors.py
@@ -1,3 +1,4 @@
+import os
 from typing import Any
 
 from django.http import HttpRequest
@@ -7,5 +8,12 @@ from country_workspace.state import state
 
 
 def current_state(request: HttpRequest) -> dict[str, Any]:
-    ret = {"state": state, "app": {"version": VERSION}}
+    ret = {
+        "state": state,
+        "app": {
+            "version": os.environ.get("VERSION", VERSION),
+            "build_date": os.environ.get("BUILD_DATE", ""),
+            "commit": os.environ.get("GIT_SHA", "-"),
+        },
+    }
     return ret
