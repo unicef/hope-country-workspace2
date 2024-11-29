e314d7978f3a81ec2eeba6430d9ad931b51f0c9d
Stefano Apostolico <s.apostolico@gmail.com>
2024-11-29 20:33:12 +0100

    updates actions

---
+++ tests/workspace/test_ws_individual.py
@@ -0,0 +1,79 @@
+from typing import TYPE_CHECKING
+
+from django.urls import reverse
+
+import pytest
+from responses import RequestsMock
+from testutils.utils import select_office
+
+from country_workspace.state import state
+
+if TYPE_CHECKING:
+    from django_webtest import DjangoTestApp
+    from django_webtest.pytest_plugin import MixinWithInstanceVariables
+
+    from country_workspace.workspaces.models import CountryIndividual
+
+pytestmark = [pytest.mark.admin, pytest.mark.smoke, pytest.mark.django_db]
+
+
+@pytest.fixture()
+def office():
+    from testutils.factories import OfficeFactory
+
+    co = OfficeFactory()
+    state.tenant = co
+    yield co
+
+
+@pytest.fixture()
+def program(office, household_checker, individual_checker):
+    from testutils.factories import CountryProgramFactory
+
+    return CountryProgramFactory(
+        household_checker=household_checker,
+        individual_checker=individual_checker,
+        household_columns="name\nid\nxx",
+        individual_columns="name\nid\nxx",
+    )
+
+
+@pytest.fixture()
+def individual(program):
+    from testutils.factories import CountryIndividualFactory
+
+    return CountryIndividualFactory(
+        household__batch__program=program, household__batch__country_office=program.country_office
+    )
+
+
+@pytest.fixture()
+def app(django_app_factory: "MixinWithInstanceVariables", mocked_responses: "RequestsMock") -> "DjangoTestApp":
+    from testutils.factories import SuperUserFactory
+
+    django_app = django_app_factory(csrf_checks=False)
+    admin_user = SuperUserFactory(username="superuser")
+    django_app.set_user(admin_user)
+    django_app._user = admin_user
+    yield django_app
+
+
+def test_ind_changelist(app: "DjangoTestApp", individual: "CountryIndividual") -> None:
+    url = reverse("workspace:workspaces_countryindividual_changelist")
+    with select_office(app, individual.country_office, individual.program):
+        res = app.get(url)
+        assert res.status_code == 200, res.location
+        assert f"Add {individual._meta.verbose_name}" not in res.text
+        # filter by program
+        res = app.get(url)
+        assert res.status_code == 200, res.location
+
+
+def test_ind_change(app: "DjangoTestApp", individual: "CountryIndividual") -> None:
+    url = reverse("workspace:workspaces_countryindividual_changelist")
+    with select_office(app, individual.country_office, individual.program):
+        res = app.get(url)
+        res = res.click(individual.name)
+        assert res.status_code == 200, res.location
+        res = res.forms["countryindividual_form"].submit()
+        assert res.status_code == 302, res.location
---
+++ tests/workspace/test_ws_program.py
@@ -0,0 +1,94 @@
+from typing import TYPE_CHECKING
+
+from django.urls import reverse
+
+import pytest
+from pytest_django.fixtures import SettingsWrapper
+from responses import RequestsMock
+from testutils.utils import select_office
+
+from country_workspace.state import state
+from country_workspace.workspaces.models import CountryHousehold, CountryProgram
+
+if TYPE_CHECKING:
+    from django_webtest import DjangoTestApp
+    from django_webtest.pytest_plugin import MixinWithInstanceVariables
+
+
+pytestmark = [pytest.mark.admin, pytest.mark.smoke, pytest.mark.django_db]
+
+
+@pytest.fixture()
+def office():
+    from testutils.factories import OfficeFactory
+
+    co = OfficeFactory()
+    state.tenant = co
+    yield co
+
+
+@pytest.fixture()
+def program(office):
+    from testutils.factories import CountryProgramFactory, DataCheckerFactory
+
+    return CountryProgramFactory(
+        country_office=office,
+        household_checker=DataCheckerFactory(fields=["collect_individual_data"]),
+        individual_checker=DataCheckerFactory(fields=["gender"]),
+        household_columns="__str__\nid\nxx",
+        individual_columns="__str__\nid\nxx",
+    )
+
+
+@pytest.fixture()
+def household(program):
+    from testutils.factories import CountryHouseholdFactory
+
+    return CountryHouseholdFactory(batch__program=program, batch__country_office=program.country_office)
+
+
+@pytest.fixture()
+def app(
+    django_app_factory: "MixinWithInstanceVariables",
+    mocked_responses: "RequestsMock",
+    settings: SettingsWrapper,
+) -> "DjangoTestApp":
+    from testutils.factories import SuperUserFactory
+
+    django_app = django_app_factory(csrf_checks=False)
+    admin_user = SuperUserFactory(username="superuser")
+    django_app.set_user(admin_user)
+    django_app._user = admin_user
+    yield django_app
+
+
+def test_configure_hh_columns(app, household: "CountryHousehold"):
+    program: "CountryProgram" = household.program
+    with select_office(app, program.country_office, program):
+        res = app.get(program.get_change_url())
+        res = res.click("Household Columns")
+        form = res.forms["configure-columns"]
+        form["columns"] = ["name", "flex_fields__collect_individual_data"]
+        form.submit().follow()
+        program.refresh_from_db()
+        assert program.household_columns == "name\nflex_fields__collect_individual_data"
+        hh_list = reverse("workspace:workspaces_countryhousehold_changelist")
+        res = app.get(hh_list)
+        assert not res.pyquery("div.text a:contains('flex_fields__collect_individual_data')")
+        assert res.pyquery("div.text a:contains('Collect_individual_data')")
+    # assert "collect_individual_data" in res.text
+
+
+def test_configure_ind_columns(app, household: "CountryHousehold"):
+    program: "CountryProgram" = household.program
+    with select_office(app, program.country_office, program):
+        res = app.get(program.get_change_url())
+        res = res.click("Individual Columns")
+        form = res.forms["configure-columns"]
+        form["columns"] = ["name", "flex_fields__gender"]
+        form.submit().follow()
+        program.refresh_from_db()
+        assert program.individual_columns == "name\nflex_fields__gender"
+        hh_list = reverse("workspace:workspaces_countryindividual_changelist")
+        res = app.get(hh_list)
+        assert "gender" in res.text
